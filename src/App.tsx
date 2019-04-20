eb5f5ec5252802727934f84112fa81a42ab30ff8
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-04-20 21:02:21 +0900

    move character

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,82 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Board from "./components/Board";
 
+type Tarn = 0 | 1;
 export interface AppProps {}
 
 export interface AppState {
-  p1: number;
-  p2: number;
+  stone: number[];
+  hWall: boolean[];
+  wWall: boolean[];
+  tarn: Tarn;
 }
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
     this.state = {
-      p1: 36,
-      p2: 44
+      stone: [36, 44],
+      hWall: Array(72).fill(false),
+      wWall: Array(72).fill(false),
+      tarn: 0
     };
+
+    this.moveCharacter = this.moveCharacter.bind(this);
   }
 
-  moveCharacter() {}
+  isWall(isW: boolean, x: any, y: any): boolean {
+    const interval = isW ? 9 : 8;
+    const index = x + y * interval;
+    return isW ? this.state.wWall[index] : this.state.hWall[index];
+  }
+
+  isEnemy(i: any, j: any, x: any, y: any): number | null {
+    // 端の判定
+    if (x + i < 0 || x + i >= 9 || y + j < 0 || y + j >= 9) {
+      return null;
+    }
+
+    // 壁判定
+    if (this.isWall(i === 0, i === 1 ? x : x + i, j === 1 ? y : y + j)) {
+      return null;
+    }
+
+    const index = x + i + 9 * (y + j);
+    const enemy = this.state.stone[this.state.tarn === 0 ? 1 : 0];
+    if (enemy === index) {
+      return this.isEnemy(i, j, x + i, y + j);
+    }
+
+    return index;
+  }
+
+  canPut(index: any): boolean {
+    const { stone, tarn } = this.state;
+    if (stone.includes(index)) {
+      return false;
+    }
+    const nums = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    const canPutPlace = [];
+    for (let i = 0; i < nums.length; i += 1) {
+      canPutPlace.push(this.isEnemy(nums[i][0], nums[i][1], stone[tarn] % 9, Math.floor(stone[tarn] / 9)));
+    }
+    return canPutPlace.includes(index);
+  }
+
+  moveCharacter(index: any) {
+    console.log("---");
+    if (this.canPut(index)) {
+      const { stone, tarn } = this.state;
+      stone[tarn] = index;
+      this.setState({ stone, tarn: tarn === 0 ? 1 : 0 });
+    }
+  }
 
   render() {
-    const { p1, p2 } = this.state;
+    const { stone, hWall, wWall } = this.state;
     return (
       <div className="app">
-        <Board p1={p1} p2={p2} />
+        <Board stone={stone} moveCharacter={this.moveCharacter} hWall={hWall} wWall={wWall} />
       </div>
     );
   }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,24 +1,38 @@
 import * as React from "react";
 
 interface BoardProps {
-  p1: number;
-  p2: number;
+  stone: number[];
+  moveCharacter: Function;
+  hWall: boolean[];
+  wWall: boolean[];
 }
 
 const Board: React.SFC<BoardProps> = props => {
-  const { p1, p2 } = props;
+  const { stone, moveCharacter, hWall, wWall } = props;
 
   const boards = [];
   for (let index = 0; index < 81; index += 1) {
     boards.push(
-      <li key={index} className="board__item">
-        {index === p1 ? <div className="stone stone_p1" /> : null}
-        {index === p2 ? <div className="stone stone_p2" /> : null}
-      </li>
+      // tslint:disable-next-line:jsx-no-lambda
+      <div key={index} className="board__item" onClick={() => moveCharacter(index)}>
+        {index}
+        {index === stone[0] ? <div className="stone stone_p1" /> : null}
+        {index === stone[1] ? <div className="stone stone_p2" /> : null}
+      </div>
     );
   }
 
-  return <ul className="board">{boards}</ul>;
+  const wWalls = [];
+  for (let index = 0; index < wWall.length; index += 1) {
+    wWalls.push(<div key={index} className={`board__w-wall ${wWall[index] ? " board__w-wall_exist" : ""}`} />);
+  }
+
+  const hWalls = [];
+  for (let index = 0; index < hWall.length; index += 1) {
+    hWalls.push(<div key={index} className={`board__h-wall ${hWall[index] ? " board__h-wall_exist" : ""}`} />);
+  }
+
+  return <div className="board">{boards}</div>;
 };
 
 export default Board;
