30bede6cc2fd5787a8cddd007baef0096569ef33
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-04-20 21:39:16 +0900

    complete logic

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -9,17 +9,33 @@ interface BoardProps {
 
 const Board: React.SFC<BoardProps> = props => {
   const { stone, moveCharacter, hWall, wWall } = props;
+  const cHWall = hWall.slice();
 
   const boards = [];
-  for (let index = 0; index < 81; index += 1) {
-    boards.push(
-      // tslint:disable-next-line:jsx-no-lambda
-      <div key={index} className="board__item" onClick={() => moveCharacter(index)}>
-        {index}
-        {index === stone[0] ? <div className="stone stone_p1" /> : null}
-        {index === stone[1] ? <div className="stone stone_p2" /> : null}
-      </div>
-    );
+  for (let ii = 0; ii < 9; ii += 1) {
+    for (let jj = 0; jj < 9; jj += 1) {
+      const index = ii * 9 + jj;
+      boards.push(
+        // tslint:disable-next-line:jsx-no-lambda
+        <div key={index} className="board__item" onClick={() => moveCharacter(index)}>
+          {index === stone[0] ? <div className="stone stone_p1" /> : null}
+          {index === stone[1] ? <div className="stone stone_p2" /> : null}
+        </div>
+      );
+      if (jj !== 8) {
+        const first = cHWall[0];
+        cHWall.shift();
+        boards.push(<div className={first ? "exist" : ""} />);
+      }
+    }
+    if (ii !== 8) {
+      for (let jj = 0; jj < 9; jj += 1) {
+        boards.push(<div className={wWall[ii * 9 + jj] ? " exist" : ""} />);
+        if (jj !== 8) {
+          boards.push(<div />);
+        }
+      }
+    }
   }
 
   const wWalls = [];
